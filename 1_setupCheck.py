843072ac3f046a8ce595be73c7f04b1474d84694
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-08-12 09:50:00 +0530

    setup succeess

--- 1_setupCheck.py
+++ 1_setupCheck.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.chrome.service import Service
 
-driver=webdriver.Chrome()
+driver = webdriver.Chrome()
 
 driver.get("https://www.youtube.com")
 driver.maximize_window()
